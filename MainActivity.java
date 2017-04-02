626a951d0cd23ee86013681cce2e6db5a32e5ca5
Jake Steinberg <jakesteinberg@gmail.com>
2017-04-01 22:32:21 -0700

    Add java files

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,23 @@
 package jws.quizselector;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import java.lang.reflect.Array;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
-String text_question = "is portland weird?";
     String false_text = "false";
     String true_text = "true";
     String correct_text = "Correct!";
@@ -21,32 +25,129 @@ String text_question = "is portland weird?";
     @Bind(R.id.questionTextView) TextView mQuestionTextView;
     @Bind(R.id.false_button) Button mFalseButton;
     @Bind(R.id.true_button) Button mTrueButton;
-    
+    @Bind(R.id.nextButton) Button mNextButton;
+    @Bind(R.id.backButton) Button mBackButton;
+    @Bind(R.id.questionImage) ImageView mQuestionImageView;
+    @Bind(R.id.returnButton) Button mReturnButton;
 
+
+    private int[] drawables = new int[] {R.drawable.civilwar, R.drawable.worldwartwo, R.drawable.constitution, R.drawable.worldwarone};
+
+    private String[] questionList = new String[] {"The Civil War was fought between the North and the South", "World War II ended in 1945", "The Constitution was Signed in 1745", "America joined world war I in 1917"};
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(questionList[0], true, drawables[0]),  new Question(questionList[1], true, drawables[1]), new Question(questionList[2], false, drawables[2]), new Question(questionList[3], true, drawables[3])
+    };
+    private int mCurrentIndex = 0;
+    private int mImageIndex = mCurrentIndex;
+    private boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+    private boolean answerIsFalse = mQuestionBank[mCurrentIndex].isAnswerFalse();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mQuestionTextView.setText(text_question);
         mFalseButton.setText(false_text);
         mTrueButton.setText(true_text);
+        mBackButton.setText("<");
+        mNextButton.setText(">");
+        mBackButton.setVisibility(View.INVISIBLE);
+        mQuestionImageView.setImageResource(drawables[mImageIndex]);
+        mReturnButton.setVisibility(View.INVISIBLE);
+
+
+
+        String question = mQuestionBank[mCurrentIndex].getTextResString();
+        mQuestionTextView.setText(question);
+
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, correct_text, Toast.LENGTH_LONG).show();
+                answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                answerIsFalse = mQuestionBank[mCurrentIndex].isAnswerFalse();
+
+                if (answerIsTrue) {
+                    Toast.makeText(MainActivity.this, correct_text, Toast.LENGTH_LONG).show();
+                }
+
+                else {
+                    Toast.makeText(MainActivity.this, wrong_text, Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, wrong_text, Toast.LENGTH_LONG).show();
+                answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                answerIsFalse = mQuestionBank[mCurrentIndex].isAnswerFalse();
+//
+                if (!answerIsFalse) {
+                    Toast.makeText(MainActivity.this, wrong_text, Toast.LENGTH_LONG).show();
+                }
+
+                if (answerIsFalse) {
+                    Toast.makeText(MainActivity.this, correct_text, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, correct_text, Toast.LENGTH_LONG);
+                }
+
             }
         });
 
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBackButton.getVisibility() == View.INVISIBLE){
+                    mBackButton.setVisibility(View.VISIBLE);
+                }
+                mCurrentIndex = (mCurrentIndex +1) % mQuestionBank.length;
+                String question = mQuestionBank[mCurrentIndex].getTextResString();
+                mQuestionTextView.setText(question);
+
+                if (mCurrentIndex == (mQuestionBank.length -1) % mQuestionBank.length ) {
+                    mNextButton.setVisibility(View.INVISIBLE);
+                    mReturnButton.setVisibility(View.VISIBLE);
+                    mBackButton.setVisibility(View.VISIBLE);
+                }
+                if (mCurrentIndex == 0) {
+                    mNextButton.setVisibility(View.VISIBLE);
+                }
+
+                mImageIndex = mCurrentIndex;
+                mQuestionImageView.setImageResource(drawables[mImageIndex]);
+
+
+            }
+        });
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex -1) % mQuestionBank.length;
+
+                if (mCurrentIndex <= 0) {
+                    mCurrentIndex = 0;
+                    mBackButton.setVisibility(View.INVISIBLE);
+                    mNextButton.setVisibility(View.VISIBLE);
+                }
+                String question = mQuestionBank[mCurrentIndex].getTextResString();
+                mQuestionTextView.setText(question);
+                mImageIndex = mCurrentIndex;
+                mQuestionImageView.setImageResource(drawables[mImageIndex]);
+            }
+        });
+
+        mReturnButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
--- Question.java
+++ Question.java
@@ -1,8 +1,45 @@
 package jws.quizselector;
 
+import android.media.Image;
+
 /**
  * Created by JS on 4/1/17.
  */
 
 public class Question {
+
+    private String mTextResString;
+    private boolean mAnswerTrue;
+
+    private int mQuestionImage;
+
+    public Question(String textResString, boolean answerTrue, int questionImage) {
+        mTextResString = textResString;
+        mAnswerTrue = answerTrue;
+        mQuestionImage = questionImage;
+    }
+
+    public String getTextResString() {
+        return mTextResString;
+    }
+
+    public void setTextResString(String textResString) {
+        mTextResString = textResString;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public boolean isAnswerFalse() {
+        return !mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public void setQuestionImage(int questionImage) {
+        mQuestionImage = questionImage;
+    }
 }
